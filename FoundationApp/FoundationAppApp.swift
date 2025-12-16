6ebb6b0c94ccdc87ac2b58b987aca9993bfbdf8a
Alessandra <alessandra.missa.bejar@gmail.com>
2025-12-16 14:14:44 +0100

    upldate the main screen, and starting working on the question screens

--- FoundationApp/FoundationAppApp.swift
+++ FoundationApp/FoundationAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct FoundationAppApp: App {
     var body: some Scene {
         WindowGroup {
-            LoadingPage()
+            
+            LoadingPage(onComplete: {})
         }
     }
 }
---
+++ FoundationApp/Functions.swift
@@ -0,0 +1,31 @@
+//
+//  Functions.swift
+//  FoundationApp
+//
+//  Created by AFP FED 41 on 15/12/25.
+//
+
+import Foundation
+import SwiftData
+import Charts
+import Combine
+
+/*
+private var selectedTags: [String] = []
+private var bedTime = Date()
+@Query private var settings: [UserSettings]
+
+func toggle(_ tag: String) {
+    if let idx = selectedTags.firstIndex(of: tag) { selectedTags.remove(at: idx) }
+    else { selectedTags.append(tag) }
+}
+
+func saveAndContinue() {
+    let user = settings.first ?? UserSettings()
+    user.sleepTroubles = selectedTags
+    user.bedTime = bedTime
+    if settings.isEmpty { context.insert(user) }
+    onContinue()
+}
+*/
+
